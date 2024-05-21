e5b22670625be4c88e5489a8e5f07be039f105c6
Breno Uchoa <uchoa.breno2016@gmail.com>
2024-05-20 20:43:14 -0400

    manipulando o servidor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-console.log('Sucesso!')
+
+const offset = 0
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+// manipula a resposta do servidor(promessa do servidor)
+fetch(url)
+
+    //transformando o response em uma promessa convertido em JSON
+    .then((response) => response.json())
+
+    //recebendo a promessa e printando
+    .then((jsonBody) => console.log(jsonBody))
+
+    //manipula o erro do servidor
+    .catch((error) => console.error(error))
