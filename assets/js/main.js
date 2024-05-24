07e984b9939425e5195e159ab50db4fb99afc6f8
Breno Uchoa <uchoa.breno2016@gmail.com>
2024-05-23 22:13:11 -0400

    adicionando mais lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,45 @@
-
-const offset = 0
+const offset = 0;
 const limit = 10;
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+function convertPokemonToLi(pokemon) {
+  return `
+    <li class="pokemon">
+      <span class="number">#001</span>
+      <span class="name">${pokemon.name}</span>
+      <div class="detail">
+        <ol class="types">
+          <li class="type">grass</li>
+          <li class="type">poison</li>
+        </ol>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+             alt="${pokemon.name}"/>
+      </div>
+    </li>
+  `;
+}
+
+
+const pokemonList = document.getElementById('pokemonList')
 
 // manipula a resposta do servidor(promessa do servidor)
 fetch(url)
+  //transformando o response em uma promessa convertido em JSON
+  .then((response) => response.json())
 
-    //transformando o response em uma promessa convertido em JSON
-    .then((response) => response.json())
+  .then((jsonBody) => jsonBody.results)
 
-    //recebendo a promessa e printando
-    .then((jsonBody) => console.log(jsonBody))
+  //recebendo a promessa e printando
+  .then((pokemons) => {
+    
+    for (let i = 0;  i < pokemons.length; i++) {
+      const pokemon = pokemons[i];
+      pokemonList.innerHTML += convertPokemonToLi(pokemon)
 
-    //manipula o erro do servidor
-    .catch((error) => console.error(error))
+    }
+
+  })
+
+  //manipula o erro do servidor
+  .catch((error) => console.error(error));
